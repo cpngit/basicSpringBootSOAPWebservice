95d886c4e4a2888bb405029ddb96e9fbf71ec5bb
cassiopereira.nunes@gmail.com <cassiopereira.nunes@gmail.com>
2020-03-22 07:24:00 +0000

    Initital Version

---
+++ src/main/java/com/cpn/spring/soap/webservices/basicSpringBootSOAPWebservice/BasicSpringBootSoapWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cpn.spring.soap.webservices.basicSpringBootSOAPWebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBootSoapWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBootSoapWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpn/spring/soap/webservices/basicSpringBootSOAPWebservice/BasicSpringBootSoapWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpn.spring.soap.webservices.basicSpringBootSOAPWebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringBootSoapWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
