2e7224a005bdadcedc49474c5dbca3e298b7c1b5
cassiopereira.nunes@gmail.com <cassiopereira.nunes@gmail.com>
2020-03-22 11:43:11 +0000

    First configuration SOAP endpoint

--- src/main/java/com/cpn/spring/soap/webservices/basicSpringBootSOAPWebservice/CourseDetailsEndPoint.java
+++ src/main/java/com/cpn/spring/soap/webservices/basicSpringBootSOAPWebservice/CourseDetailsEndPoint.java
@@ -1,4 +1,23 @@
 package com.cpn.spring.soap.webservices.basicSpringBootSOAPWebservice;
 
+import com.cpn.ws.GetCourseDetailsRequest;
+import com.cpn.ws.GetCourseDetailsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
 public class CourseDetailsEndPoint {
+
+    @PayloadRoot(namespace="http://www.cpn.com/ws", localPart="GetCourseDetailsRequest")
+    @ResponsePayload
+    public GetCourseDetailsResponse processCourseDetailRequest(@RequestPayload GetCourseDetailsRequest request){
+        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+
+        return response;
+    }
+
+
 }
